add4f0e0cad6ce340e9cdd2193908e3d64e67dc3
Flere21 <aaronhans2134@gmail.com>
2023-04-24 12:20:42 +0800

    pseudocode

---
+++ multplelinetxt.py
@@ -0,0 +1,8 @@
+#write line to file function
+#create a list for lines
+#ask user to write a line of text
+#add line of text to list
+#ask user if wants to write another line
+#if yes write and add another line of text
+#if no end program
+#open mylife.txt for writing
