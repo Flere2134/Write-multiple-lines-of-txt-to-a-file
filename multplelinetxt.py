46b2f4c1c531196e97ca6a3e4980b6e724f0a920
Flere21 <aaronhans2134@gmail.com>
2023-04-24 12:24:13 +0800

    ask for user input

--- multplelinetxt.py
+++ multplelinetxt.py
@@ -3,6 +3,7 @@ def write_line_to_file():
 #create a list for lines
     lines = []
 #ask user to write a line of text
+    text = input('Write something you want to say or ask: ')
 #add line of text to list
 #ask user if wants to write another line
 #if yes write and add another line of text
